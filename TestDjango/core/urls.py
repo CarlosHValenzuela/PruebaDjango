9f06c5e16e00cc7efc1fee97de5ab97d92194120
CarlosHValenzuela <carl.valenzuela@duocuc.cl>
2022-06-01 14:53:51 -0400

    Cambios en Diseño y Todas las paginas funcionan, Tambien hay que mejorar el diseño

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import Registro 
+from .views import Registro,Pedido,InicioSesion,Carrito,Registrate,MetodoPago,Suscribirte
 
 urlpatterns =[
     path('', Registro,name="home"),
+    path('Pedido',Pedido,name="Pedido"),
+    path('InicioSesion',InicioSesion,name="InicioSesion"),
+    path('Carrito',Carrito,name="Carrito"),
+    path('Registrate',Registrate,name="Registrate"),
+    path('MetodoPago',MetodoPago,name="MetodoPago"),
+    path('Suscribirte',Suscribirte,name="Suscribirte"),
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def Registro(request):
     return render(request,'core/Registro.html')
+
+def Pedido(request):
+    return render(request,'core/Pedido.html')
+
+def InicioSesion(request):
+    return render(request,'core/InicioSesion.html')
+
+def Carrito(request):
+    return render(request,'core/Carrito.html')
+
+def Registrate(request):
+    return render(request,'core/Registrate.html')
+
+def MetodoPago(request):
+    return render(request,'core/MetodoPago.html')
+
+def Suscribirte(request):
+    return render(request,'core/Suscribirte.html')
