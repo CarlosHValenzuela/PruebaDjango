d7d503e4ce30c23eeafd7b87772cb7d3bb1d6e19
CarlosHValenzuela <carl.valenzuela@duocuc.cl>
2022-06-03 11:56:03 -0400

    Merge branch 'main' of https://github.com/CarlosHValenzuela/PruebaDjango into main

--- TestDjango/core/models.py
+++ TestDjango/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cliente(models.Model):
+    id_Cliente =models.IntegerField(primary_key=True, verbose_name='Id de cliente')
+    nom_Usuario =models.CharField(max_length=100, verbose_name='Nombre de usuario')
+    pass_Usuario =models.CharField(max_length=50, verbose_name='PassWord de usuario')
+    email_Usuario =models.CharField(max_length=100, verbose_name='Correo del usuario')
+
+    def __str__(self):
+        return self.nom_Usuario
